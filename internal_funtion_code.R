8dbc8db7957d371cc3bd524ada5bb4cb2416b87d
Victor <VictorYu@sent.com>
2020-06-15 01:07:12 +0100

    minor fix

--- internal_funtion_code.R
+++ internal_funtion_code.R
@@ -154,7 +154,7 @@ Pull_From_SQL <- function(Provider_Code, Provider_Code_00, Specialty, Treatment_
       if (Specialty == "X01") {
         sql(paste0("AND Specialty NOT IN (''", outpatients_regex, "'') "))
       } else {
-      paste0("AND Specialty = '", as.character(Specialty), "' ")
+      paste0("AND Specialty = ''", as.character(Specialty), "'' ")
       },
       "' )",
       con = con
@@ -178,7 +178,7 @@ Pull_From_SQL <- function(Provider_Code, Provider_Code_00, Specialty, Treatment_
       if (Specialty == "X01") {
         sql(paste0("AND Specialty NOT IN (''", outpatients_regex, "'') "))
       } else {
-        paste0("AND Specialty = '", as.character(Specialty), "' ")
+        sql(paste0("AND Specialty = ''", as.character(Specialty), "'' "))
       },
       "' )",
       con = con
